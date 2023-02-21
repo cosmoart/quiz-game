6fabb351e2d0dbfc2d97e83886410f76730d85aa
CosmoArt <cosmohydra17@gmail.com>
2023-02-20 21:47:56 -0500

    api on

--- source_code/src/helpers/getQuestions.js
+++ source_code/src/helpers/getQuestions.js
@@ -13,56 +13,56 @@ export default async function getQuestions (topicsArray, questionsNumber) {
 		return newArr
 	}
 
-	// return new Promise((resolve, reject) => {
-	// 	const questions = []
-	// 	const topics = []
-
-	// 	for (let i = 0; i < questionsNumber; i++) {
-	// 		topics.push(topicsArray[i % topicsArray.length])
-	// 	}
-
-	// 	Promise.all(random(topics).map(async (topic) => {
-	// 		const response = await cohere.generate({
-	// 			model: 'xlarge',
-	// 			prompt: promts[topic],
-	// 			max_tokens: 65,
-	// 			temperature: 0.3,
-	// 			k: 0,
-	// 			p: 0.75,
-	// 			stop_sequences: ['--'],
-	// 			frequency_penalty: 0.9,
-	// 			presence_penalty: 0.36,
-	// 			return_likelihoods: 'NONE'
-	// 		})
-	// 		if (response.statusCode >= 400) return reject(response)
-	// 		return response
-	// 	})).then((responses) => {
-	// 		try {
-	// 			responses.forEach((response, i) => {
-	// 				const res = response.body.generations[0].text
-	// 				questions.push({
-	// 					topic: topics[i],
-	// 					question: res.split('\n')[1].split('Question: ')[1],
-	// 					answers: random(res.split('\n').slice(2, 6).map((answer) => answer.split('- ')[1])),
-	// 					correctAnswer: res.split('\n')[6].split('Correct: ')[1],
-	// 					userAnswer: 0
-	// 				})
-	// 			})
-	// 		} catch (err) {
-	// 			reject(err || { message: 'Something went wrong' })
-	// 		}
-	// 	}).catch(err => {
-	// 		reject(err[0] || { message: 'Something went wrong' })
-	// 	}).then(() => resolve(questions))
-	// })
-
 	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			// const error = new Error('Custom error')
-			// error.statusCode = 350
-			// error.body = { message: 'Custom error' }
-			// reject(error)
-			resolve(random(testQuestions).slice(0, questionsNumber))
-		}, 1000)
+		const questions = []
+		const topics = []
+
+		for (let i = 0; i < questionsNumber; i++) {
+			topics.push(topicsArray[i % topicsArray.length])
+		}
+
+		Promise.all(random(topics).map(async (topic) => {
+			const response = await cohere.generate({
+				model: 'xlarge',
+				prompt: promts[topic],
+				max_tokens: 65,
+				temperature: 0.3,
+				k: 0,
+				p: 0.75,
+				stop_sequences: ['--'],
+				frequency_penalty: 0.9,
+				presence_penalty: 0.36,
+				return_likelihoods: 'NONE'
+			})
+			if (response.statusCode >= 400) return reject(response)
+			return response
+		})).then((responses) => {
+			try {
+				responses.forEach((response, i) => {
+					const res = response.body.generations[0].text
+					questions.push({
+						topic: topics[i],
+						question: res.split('\n')[1].split('Question: ')[1],
+						answers: random(res.split('\n').slice(2, 6).map((answer) => answer.split('- ')[1])),
+						correctAnswer: res.split('\n')[6].split('Correct: ')[1],
+						userAnswer: 0
+					})
+				})
+			} catch (err) {
+				reject(err || { message: 'Something went wrong' })
+			}
+		}).catch(err => {
+			reject(err[0] || { message: 'Something went wrong' })
+		}).then(() => resolve(questions))
 	})
+
+	// return new Promise((resolve, reject) => {
+	// 	setTimeout(() => {
+	// 		// const error = new Error('Custom error')
+	// 		// error.statusCode = 350
+	// 		// error.body = { message: 'Custom error' }
+	// 		// reject(error)
+	// 		resolve(random(testQuestions).slice(0, questionsNumber))
+	// 	}, 1000)
+	// })
 }
